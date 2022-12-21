5d8dbbf80ae5294ccb7aac1ee1bb1dcbb5b0a3fe
Lê Minh Quân <marucube35@gmail.com>
2022-12-21 13:17:33 +0700

    misc: add converter to change pascal case rule name to regular name

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace BatchRename
 {
---
+++ PascalCaseToRegular.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BatchRename
+{
+    internal class PascalCaseToRegularConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var pascalCase = (string)value;
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < pascalCase.Length; i++)
+            {
+                var c = pascalCase[i];
+
+                if (char.IsUpper(c) && i > 0)
+                {
+                    builder.Append(' ');
+                    builder.Append(char.ToLower(c));
+                }
+                else
+                {
+                    builder.Append(c);
+                }
+            }
+
+            var result = builder.ToString();
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
